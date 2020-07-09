0a50d9083706de2c8f141fd8e53fe8588cd5bfbf
Maxime <maxime_hemmerle@orange.fr>
2020-07-09 09:21:56 +0200

    fin

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+
+
+
+/**
+   * @Route("/categories/{id}", name="categories")
+   */
+
+  public function show(Request $request, PaginatorInterface $paginator, $id)
+  {
+    $category = $this->getDoctrine()->getRepository(Categories::class)->find($id);
+
+    if (!$category) {
+      return $this->redirectToRoute('movies_list');
+    }
+    $pagination = $paginator->paginate(
+      $category->getMovies(), // Requête contenant les données à paginer (ici nos articles)
+      $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+      18 // Nombre de résultats par page
+    );
+
+    return $this->render('categories/index.html.twig', [
+      'categories' => $pagination,
+      'category' => $category,
+    ]);
+
+  }
+}
--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use Knp\Component\Pager\PaginatorInterface;
+
 use App\Entity\Movies;
 use App\Controller\MoviesController;
 
@@ -13,15 +17,22 @@ class MoviesController extends AbstractController
     /**
      * @Route("/movies", name="movies")
      */
-    public function index()
+    public function index(PaginatorInterface $paginator, Request $request)
     {
         $movies = $this->getDoctrine()
          ->getRepository(Movies::class)
-         ->findBy([], ['name' => 'ASC'], 20, 0);
+         ->findBy([], ['name' => 'ASC']);
 
-        return $this->render('movies/index.html.twig', [
-          'movies' => $movies,
-        ]);
+		     $pagination = $paginator->paginate(
+		       $movies, // Requête contenant les données à paginer (ici nos articles)
+		       $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+		       18 // Nombre de résultats par page
+		     );
+
+		     return $this->render('movies/index.html.twig', [
+		       'categories' => $pagination,
+		       'movies' => $movies,
+		     ]);
 
 
 
